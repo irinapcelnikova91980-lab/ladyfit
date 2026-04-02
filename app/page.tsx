30f2ed658c58e8d88fc05e2e478e38c169a55365
irinapcelnikova91980-lab <irinapcelnikova91980@gmail.com>
2026-04-02 14:01:52 +0300

    init ladyfit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">LadyFit</h1>
+      <p className="mt-4">Платформа фитнес-тренера</p>
+    </main>
+  )
+}
